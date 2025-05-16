1d558d61d559c078b4fac426962507d130ca2c74
serenatrovalusci <trovalusci.2128733@studenti.uniroma1.it>
2025-05-16 12:55:32 +0200

    updates

--- train_AE.py
+++ train_AE.py
@@ -85,7 +85,7 @@ train_loader, test_loader = train_test_split(Y, test_size=0.2, random_state=42)
 train_loader = DataLoader(TensorDataset(torch.FloatTensor(train_loader)), batch_size=64, shuffle=True)
 test_loader = DataLoader(TensorDataset(torch.FloatTensor(test_loader)), batch_size=64)
 
-model = HandPoseAE(input_dim=input_dim, latent_dim=20)
+model = HandPoseAE(input_dim=input_dim, latent_dim=45)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10, factor=0.5, verbose=True)
 loss_fn = nn.MSELoss()
